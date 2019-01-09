faed777e1e0eff6096108bf8828c662a6fccdd0d
sigmaray <sigmaray@gmail.com>
2019-01-10 00:44:33 +0300

    cweather.api namespace

---
+++ src/cweather/api.clj
@@ -0,0 +1,31 @@
+(ns cweather.api
+  (:require [clj-http.client :as client]
+    [clojure.data.json :as json]))
+
+; (def api-key "b6907d289e10d714a6e88b30761fae22")
+(def api-key "5a043a1bd95bf3ee500eb89de107b41e")
+
+(defn get-weather
+  ([] (get-weather "London"))
+  ([location] 
+    (json/read-str
+      (:body 
+        (client/get
+          (str "https://api.openweathermap.orgg/data/2.5/find?q=" location "&units=metric&appid=" api-key))))
+  )
+)
+
+(defn get-temp
+  ([] (get-temp "London"))
+  ([location]
+    (try
+      (Thread/sleep 4000)
+      (((((get-weather location) "list") 0) "main") "temp")
+      (catch Exception e (str "Couldn't get weather. Caught exception: " (.getMessage e)))
+    )    
+  )
+)
+
+(defn get-name []
+  ((((get-weather) "list") 0) "name")
+)
--- src/cweather/core.clj
+++ src/cweather/core.clj
@@ -1,40 +1,10 @@
 (ns cweather.core (:gen-class)
-  (:require [clj-http.client :as client]
-    [clojure.data.json :as json]))
+  (:use cweather.api))
 
-; (def api-key "b6907d289e10d714a6e88b30761fae22")
-(def api-key "5a043a1bd95bf3ee500eb89de107b41e")
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-
-(defn get-weather
-  ([] (get-weather "London"))
-  ([location] 
-    (json/read-str
-      (:body 
-        (client/get
-          (str "https://api.openweathermap.orgg/data/2.5/find?q=" location "&units=metric&appid=" api-key))))
-  )
-)
-
-(defn get-temp
-  ([] (get-temp "London"))
-  ([location]
-    (try
-      (Thread/sleep 4000)
-      (((((get-weather location) "list") 0) "main") "temp")
-      (catch Exception e (str "Couldn't get weather. Caught exception: " (.getMessage e)))
-    )    
-  )
-)
-
-(defn get-name []
-  ((((get-weather) "list") 0) "name")
-)
+; (defn foo
+;   "I don't do a whole lot."
+;   [x]
+;   (println x "Hello, World!"))
 
 ; (defn -main []
 ;    (println "I'm a little teapot!"))
