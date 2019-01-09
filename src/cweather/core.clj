4ab89390887220c477262cccada0524ab2cba01a
sigmaray <sigmaray@gmail.com>
2019-01-09 23:07:20 +0300

    main

--- src/cweather/core.clj
+++ src/cweather/core.clj
@@ -1,6 +1,9 @@
-(ns cweather.core)
+(ns cweather.core (:gen-class))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main []
+   (println "I'm a little teapot!"))
